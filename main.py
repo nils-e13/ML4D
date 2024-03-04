8ab3ab2d47cc459b718e48275820cc1388da30ba
nils-e13 <92092665+nils-e13@users.noreply.github.com>
2024-03-04 18:03:19 +0100

    feat: added filenames array

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ import sys
 import json
 
 from Library import lib
-
 from dotenv import load_dotenv
 load_dotenv()
 
 API_TOKEN = os.getenv['API_TOKEN']
 API_URL = 'https://api-inference.huggingface.co/models/facebook/detr-resnet-50'
 
-# Folder containing images, select bikes, cars, motorbikes here
+# folder containing images, select bikes, cars, motorbikes here
 folder_path = 'data/bike/1'
 
 # set up variables
 total_images = 0
 vehicle_count = 0
+vehicle_images = []
 not_vehicle_count = 0
+not_vehicle_images = []
 # specify type of vehicle here (car, bicycle, motorbike)
 vehicle = "bicycle"
 
-# Go over each image in the folder
+# go over each image in the folder
 for filename in os.listdir(folder_path):
-    # Check filetype
+    # check filetype
     if filename.endswith('.jpeg') or filename.endswith('.jpg') or filename.endswith('.png'):
-        # Create image path
+        # create image path
         image_path = os.path.join(folder_path, filename)
 
-        # Classify the image
+        # classify the image
         try:
             # receive image data and store reponse
             response = lib.query(image_path, API_URL, API_TOKEN)
@@ -41,19 +42,23 @@ for filename in os.listdir(folder_path):
             # draw bounding boxes around specified vehicle
             lib.draw_rect(image_path, API_URL, API_TOKEN, label=vehicle)
 
-            # Increment total_images counter
+            # increment total_images counter
             total_images += 1
 
-            # Check if the image was classified as specified vehicle and increment counters accordingly
+            # check if the image was classified as specified vehicle and increment counters accordingly
             if any(vehicle in prediction['label'].lower() for prediction in response):
                 vehicle_count += 1
+                vehicle_images.append(filename)
             else:
                 not_vehicle_count += 1
+                not_vehicle_images.append(filename)
 
+        # if error detected, print to console
         except Exception as e:
             print(f"An error occurred while processing {image_path}: {e}")
 
-# Print the final counts
+# print the final counts + filenames
 print(f"Total images: {total_images}")
 print(f"Classified as containing {vehicle}: {vehicle_count}")
 print(f"Not classified as containing {vehicle}: {not_vehicle_count}")
+print(f"Not classified as containing {vehicle}: {not_vehicle_images}")
